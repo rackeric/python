3cb81355a9f582ac6f7a9aecbc449e06fc54d31a
rackeric <utcodemonkey@hotmail.com>
2013-04-12 01:40:51 +0000

    update

--- create-from-snap.py
+++ create-from-snap.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python
+#
+# Creates a CS Server from a snapshot image
+# Creates a CBS volume and attaches to new server 
+#
+# 20130411 - Eric Hernandez
+#
 
 import pyrax 
 import time
 
-username = "utcodemonkey"
-API_key = "25136ca5cbe07aa925da1ee2f232aa5a"
-
-pyrax.set_credentials(username, API_key)
-
-cs = pyrax.cloudservers
-cbs = pyrax.cloud_blockstorage
-
 ## VALUES TO CHANGE
-cs_snap_uuid = "039a39dc-3000-4751-b79f-f7158772cf51"
-cbs_snap_uuid = "7a97fc49-5200-4e7d-8a95-3d3d4cd119e6"
+username = ""
+API_key = ""
+cs_snap_uuid = ""
+cbs_snap_uuid = ""
 new_server_name = "newserver"
 new_volume_name = "newvolume"
 flavorID = 2
 cbs_size = 100
+
+# authenticate
+pyrax.set_credentials(username, API_key)
+
+# create objects
+cs = pyrax.cloudservers
+cbs = pyrax.cloud_blockstorage
 
 # create CBS volumes
 vol = cbs.create(new_volume_name, size=cbs_size, volume_type="SATA", snapshot_id = cbs_snap_uuid)
@@ -29,11 +36,6 @@ server = cs.servers.create(new_server_name, cs_snap_uuid, flavorID)
 print "-Server created."
 print "  Server ID: ", server.id
 print "  Admin password: ", server.adminPass
-
-# wait for networks to be assigned
-#while not (server.networks):
-#	server = cs.servers.get(server.id)
-#	time.sleep(10)
 
 # wait for volume to build
 mountpoint = "/dev/xvdb"
